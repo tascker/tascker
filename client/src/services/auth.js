bd3af5cc29222f4c84328d252e2e042174b3d030
Veronica De Leon <v.dleon@gmail.com>
2020-10-01 14:04:38 +0200

    signup

---
+++ client/src/services/auth.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const signup = (username, email, password, department) => {
+  return axios
+    .post("/api/auth/signup", { username, email, password, department })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      return error.response.data;
+    });
+};
+
+const login = (username, password) => {
+  return axios
+    .post("/api/auth/login", { username, password })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      return error.response.data;
+    });
+};
+
+const logout = (username, password) => {
+  return axios
+    .post("/api/auth/logout")
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      return error.response.data;
+    });
+};
+
+export { signup, login, logout };
