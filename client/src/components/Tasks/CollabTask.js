ef89d111aa0479ca0941dbe1d008db5d418b587f
katrin panova <katrin.panova@gmail.com>
2020-10-05 13:51:18 +0200

    navbar

--- client/src/components/Tasks/CollabTask.js
+++ client/src/components/Tasks/CollabTask.js
@@ -1,41 +1,42 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import TaskList from "../TaskList/TaskList";
 
 export default class CollabTask extends Component {
-    state = {
-        tasks: [],
-    };
+  state = {
+    tasks: [],
+  };
 
-    componentDidMount() {
-        this.getTasksFromDB();
-    }
+  componentDidMount() {
+    this.getTasksFromDB();
+  }
 
-    getTasksFromDB = () => {
-        const userId = this.props.user
-        console.log("userId", userId)
-        axios
-            .get("/api/tasks")
-            .then((response) => {
-                console.log("in Task response", response);
-                const filtered = response.data.filter(res => res.collaborators === userId._id)
-                //   console.log("filetered data", filtered)
-                this.setState({
-                    tasks: filtered,
-                });
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    };
-
-    render() {
-        return (
-            <div>
-                <TaskList tasks={this.state.tasks} />
-            </div>
+  getTasksFromDB = () => {
+    const userId = this.props.user;
+    console.log("userId", userId);
+    axios
+      .get("/api/tasks")
+      .then((response) => {
+        console.log("in Task response", response);
+        const filtered = response.data.filter(
+          (res) => res.collaborators === userId._id
         );
-    }
-}
+        //   console.log("filetered data", filtered)
+        this.setState({
+          tasks: filtered,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
+  render() {
+    return (
+      <div>
+        <TaskList tasks={this.state.tasks} />
+      </div>
+    );
+  }
+}
