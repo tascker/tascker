63d69d4f9904f54b7a412b3dd9bf793c3a3d5d31
Veronica De Leon <v.dleon@gmail.com>
2020-10-08 17:59:53 +0200

    server deployment

--- app.js
+++ app.js
@@ -75,7 +75,8 @@ app.use(
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
@@ -92,5 +93,10 @@ app.use("/api/user", user);
 
 const auth = require("./routes/auth");
 app.use("/api/auth", auth);
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 module.exports = app;
